1e915617ce7130be95323d51247fbb7c3a885f4b
Ian Hill <ian.g.hill@outlook.com>
2024-10-16 16:34:01 +0100

    Treemaps

--- treemaps.py
+++ treemaps.py
@@ -23,3 +23,36 @@ fig = px.treemap(
 )  # Title of the treemap
 
 fig.show()
+
+# Sales data
+data = {
+    "Category": [
+        "Electronics",
+        "Electronics",
+        "Electronics",
+        "Furniture",
+        "Furniture",
+        "Furniture",
+        "Clothing",
+        "Clothing",
+        "Clothing",
+    ],
+    "Subcategory": [
+        "Laptops",
+        "Smartphones",
+        "Tablets",
+        "Chairs",
+        "Tables",
+        "Sofas",
+        "Men",
+        "Women",
+        "Kids",
+    ],
+    "Sales": [120000, 80000, 30000, 50000, 40000, 20000, 70000, 90000, 40000],
+}
+df = pd.DataFrame(data)
+# Creating the treemap
+fig = px.treemap(
+    df, path=["Category", "Subcategory"], values="Sales", title="Sales Data Treemap"
+)
+fig.show()
